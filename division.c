9e0536134f5ab9429ff20656668594ddc11581dc
Sujal Mahind <165544809+sujalmahind@users.noreply.github.com>
2025-01-15 22:19:46 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main();
+{
+ int num1,num2,result;
+ clrscr();
+ printf("enter two number for division");
+ scanf("%d",&num1);
+ printf("enter another number");
+ scanf("%d",&num2);
+ result=num1*num2;
+ printf("division is %d",result);
+ getch();
+}
---
+++ substraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num1,num2,result;
+ clrscr();
+ printf("enter a number:");
+ scanf("%d",&num1);
+ printf("enter another number:");
+ scanf("%d",&num2);
+ result=num1-num2;
+ printf("substraction is %d",result);
+ getch();
+}
