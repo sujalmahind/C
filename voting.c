962d0b611d15533b864ce6261afa924b7ee29431
Sujal Mahind <165544809+sujalmahind@users.noreply.github.com>
2025-01-17 07:53:57 +0530

    Add files via upload

---
+++ voting.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int age;
+ clrscr()
+ print(“enter your age to find the eligibility for voting:”);
+ scanf(“%d”,&age);
+ if(age<18){
+ printf(“you are not eligible!”);
+ }
+ Else{
+ Printf(“you are eligible for voting”);
+ }
+ Getch();
+}
